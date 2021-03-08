fd371f4b95c62662f567cca61e22ae84a8b560b6
jeff1haupt <jhaupt@southbendin.gov>
2021-03-08 06:07:54 -0500

    continuing updates to codewars problem

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ public class App {
 
     public static void main(String[] args) {
 
-        String game1 = "1/ 11 11 11 1/ 11 11 11 11 11";
+        String game1 = "11 11 11 11 11 11 11 11 X 11";
         String game2 = "X X X X X X X X X XXX";
 
         bowlingScore(game1);
@@ -29,40 +29,56 @@ public class App {
         String tempString;
 
         for (int i = 1; i <= 10; i++) {
-            tempString = frames.get(i);
-            if ( !tempString.contains("X") && !tempString.contains("/") ) {
-                tempChar = tempString.charAt(0);
-                tempScore += Character.getNumericValue(tempChar);
-                tempChar = tempString.charAt(1);
-                tempScore += Character.getNumericValue(tempChar);
-            } else if ( tempString.contains("/") ) {
-                tempScore += 10;
-                tempString = frames.get(i + 1);
-                tempChar = tempString.charAt(0);
-                if ( tempChar == 'X' ) {
-                    tempScore += 10;
-                } else {
-                    tempScore += Character.getNumericValue(tempChar);
+            if ( i == 10 ) {
+                tempString = frames.get(i);
+                for ( int s = 0; s < tempString.length(); s++ ) {
+                    tempChar = tempString.charAt(s);
+                    if ( tempChar == 'X' ) {
+                        tempScore += 10;
+                        if ( (s == 0 || s == 1) && (frames.get(i-1).charAt(0) == 'X')) {
+                            tempScore += 10;
+                        }
+                        if ( s == 0 && frames.get(i-1).contains("/")) {
+// finish out 10th frame scoring
+                        }
+                    } else if ( tempChar == '/' ) {
+                        tempScore += 10 - Character.getNumericValue(tempString.charAt(s-1));
+                    } else {
+                        tempScore += Character.getNumericValue(tempChar);
+                        // this needs to check to see if 9th frame was a strike or a spare too
+                    }
                 }
             } else {
-                tempScore += 10;
-                if ( i < 9 ) {
-                    for ( int k = 1; k <= 2; k++ ) {
-                        tempString = frames.get(i + k);
-                        tempChar = tempString.charAt(0);
-                        if ( tempChar == 'X' ) {
-                            tempScore += 10;
-                        } else {
-                            tempScore += Character.getNumericValue(tempChar);
+                tempString = frames.get(i);
+                if ( !tempString.contains("X") && !tempString.contains("/") ) {
+                    tempChar = tempString.charAt(0);
+                    tempScore += Character.getNumericValue(tempChar);
+                    tempChar = tempString.charAt(1);
+                    tempScore += Character.getNumericValue(tempChar);
+                } else if ( tempString.contains("/") ) {
+                    tempScore += 10;
+                    tempString = frames.get(i + 1);
+                    tempChar = tempString.charAt(0);
+                    if ( tempChar == 'X' ) {
+                        tempScore += 10;
+                    } else {
+                        tempScore += Character.getNumericValue(tempChar);
+                    }
+                } else {
+                    tempScore += 10;
+                    if ( i < 9 ) {
+                        for ( int k = 1; k <= 2; k++ ) {
+                            tempString = frames.get(i + k);
+                            tempChar = tempString.charAt(0);
+                            if ( tempChar == 'X' ) {
+                                tempScore += 10;
+                            } else {
+                                tempScore += Character.getNumericValue(tempChar);
+                            }
                         }
                     }
-
-
                 }
-
-
             }
-
         }
         System.out.println(tempScore);
         return 0;
